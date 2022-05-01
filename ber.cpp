e4249426e05d2adf6a0c36d07ee2d86762646865
unknown <k.koziolek98@gmail.com>
2022-05-01 09:33:30 +0200

    Adding definition of function compareOfBits

--- ber.cpp
+++ ber.cpp
@@ -1,40 +1,53 @@
-#include<iostream>
+#include <iostream>
 #include <fstream>
 #include <chrono>
+#include <bitset>
+
+void compareOfBits(std::bitset <8>  a, std::bitset <8>  b, int& counterBits, int& counterDiffBits)
+{
+    for (int i = 0; i < 8; ++i)
+    {
+        counterBits++;
+        if (a[i] != b[i]) counterDiffBits++;
+    }
+};
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Bit Error Rate " << std::endl;
+  std::cout << "Bit Error Rate" << std::endl;
+
     std::ofstream output ("log.txt");
 
-     if(argc != 3) 
+     if (argc != 3) 
     {
         std::cout << "You must give two arguments!" << std::endl;
         return 1;
     }
-
+    
     std::ifstream input1 (argv[1]);
-    if(!input1.good())
+    if (!input1.good())
     {   
         std::cout << "First input file isn't exist!" << std::endl;
         return 1;
     }
 
     std::ifstream input2 (argv[2]);
-    if(!input2.good())
+    if (!input2.good())
     {
         std::cout << "Second input file isn't exist!" << std::endl;
         return 1;
     }
-    
-    int counterBits{};
+
+    int counterBits{-8};
     int counterDiffBits{};
     auto timeCalStart = std::chrono::system_clock::now();
 
     while (!input1.eof())
     {
-       // TO DO
-       // compare of bits
+        std::bitset<8> a, b;
+        input1 >> a;
+        input2 >> b;
+        compareOfBits(a, b, counterBits, counterDiffBits);
     }
 
     auto timeCalEnd = std::chrono::system_clock::now();
@@ -44,7 +57,6 @@ int main(int argc, char* argv[])
               << "Number of different bits: " << counterDiffBits << std::endl
               << "BER: " << std::boolalpha << static_cast<bool>(!counterDiffBits) << std::endl
               << "Calculations time: " << diffTime.count() << " secund" << std::endl;
-
 
     output.close();
     input1.close();
